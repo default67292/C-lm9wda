ff383fb1ecfe0c2e824cb0ed737693d633f01631
jeff <mohammedchibati@gmail.com>
2026-03-01 00:24:45 +0000

    typ shi

---
+++ sick/sick.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+ 
+
+
+
+ int miss (int n1 , int n2 ){
+ 
+  int result = n1 + n2 ;
+
+	  return result;
+ 
+ 
+ 
+ }
+
+ int main (){
+ 
+ 
+   int m = miss (99 , 90);
+
+    printf("%i|n", m   );
+ 
+ 
+ 
+ 
+ }
